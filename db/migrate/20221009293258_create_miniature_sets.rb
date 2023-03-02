cf993b8c0dbac71f79b659a97acd8f5c103a7e34
Glen <glen.kimani@student.moringaschool.com>
2023-03-02 16:06:44 +0300

    migrations complete

---
+++ db/migrate/20221009293258_create_miniature_sets.rb
@@ -0,0 +1,9 @@
+class CreateMiniatureSets < ActiveRecord::Migration[6.1]
+    def change
+      create_table :miniature_sets do |t|
+        t.string :name
+        t.integer :year
+      end
+    end
+  end
+  
